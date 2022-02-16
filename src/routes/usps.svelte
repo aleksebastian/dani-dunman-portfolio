7b62c73b2fa48afe06a06d32a0d99acd2639c41f
Alek <aleksebastian@gmail.com>
2022-02-15 21:22:15 -0600

    Implemented moreResearch component in usps route

--- src/routes/usps.svelte
+++ src/routes/usps.svelte
@@ -8,6 +8,7 @@
 	import Painpoints from '../components/usps/Painpoints.svelte';
 	import ImageLoader from '../components/image/ImageLoader.svelte';
 	import Expandable from '../components/Expandable.svelte';
+	import MoreResearch from '../components/usps/MoreResearch.svelte';
 
 	let percentage = 0;
 	$: showStatistics = false;
@@ -137,99 +138,7 @@
 
 		<!-- MORE RESEARCH -->
 		{#if showResearch}
-			<div transition:slide={{ duration: 1000 }} class="full grid">
-				<div class="moreResearch py-md">
-					<p class="researchText label pb-sm">Competitive Audit</p>
-					<p class="researchText py-sm">There have been proposals to solve this issue.</p>
-					<div class="flex-row proposals py-sm">
-						<div class="proposal">
-							<p>{'Cryptocurrencies'.toUpperCase()}</p>
-							<p>
-								are fully digital and easy to access, and location and income are not a barrier.
-							</p>
-						</div>
-						<div class="proposal">
-							<p>{'“Outlet” bank branch offices'.toUpperCase()}</p>
-							<p>
-								are branches of participating banks that are conveniently located for lower-income
-								households.
-							</p>
-						</div>
-						<div class="proposal">
-							<p>{'Neobanks'.toUpperCase()}</p>
-							<p>
-								a type of direct bank that functions fully online, such as Chime, Current, and
-								Aspiration.
-							</p>
-						</div>
-					</div>
-					<p class="researchText">However,</p>
-					<p class="researchText">there are no active soluctions.</p>
-				</div>
-
-				<!-- UX RESEARCH METHODS -->
-				<div class="flex-row content ux-methods py-md flex-wrap">
-					<div class="image">
-						<ImageLoader alt="rectangle" src={'https://picsum.photos/800/500'} />
-					</div>
-					<div class="text">
-						<p class="label py-sm">UX RESEARCH METHOD 01: How Might We (HMW’s)</p>
-						<p>
-							My team and I created a Miro board where we could share our research findings and
-							organize them in such a way that we could easily compare and contrast between
-							different questions and problem spaces. We looked over our HMWs and reorganized our
-							sticky notes into the categories of information distribution, resources/education,
-							security, utility services, and tasks. From there, we sorted through them once again
-							and created another table with the most relevant and helpful questions. In this table,
-							we listed possible answers to the HMWs.
-						</p>
-					</div>
-				</div>
-				<div class="flex-row content ux-methods py-md flex-wrap">
-					<div class="text">
-						<p class="label py-sm">UX RESEARCH METHOD 02: Survey</p>
-						<p>
-							I created a quick survey that I shared on my Instagram for people to answer questions
-							about their experience with banking, their frustrations, and what they would be
-							willing to do to open a bank account if they found themselves on the bank's deny-list.
-						</p>
-						<p class="py-md">
-							Those who are denied standard bank accounts due to a rocky banking history were
-							identified as part of the unbanked community, and are the target audience.
-						</p>
-					</div>
-
-					<div class="complex">
-						<div class="imgs flex-row">
-							<div class="sq-img">
-								<ImageLoader alt="square" src={`https://picsum.photos/600`} />
-							</div>
-							<div class="sq-img">
-								<ImageLoader alt="square" src={`https://picsum.photos/600`} />
-							</div>
-						</div>
-						<div class="img">
-							<ImageLoader alt="square" src="https://picsum.photos/800/300" />
-						</div>
-					</div>
-				</div>
-
-				<div class="flex-row content ux-methods last flex-wrap">
-					<img alt="rectangle" src="https://picsum.photos/750/450" class="image" />
-					<div class="text">
-						<p class="label py-sm">UX RESEARCH METHOD 03/04: User Persona and Journey Map</p>
-						<p>
-							My target audience is deny-listed, bank users. People who have been kicked out of the
-							banking system are unable to open an account independent of where they go because
-							their history follows them. Not being able to bank pushes them further into
-							“unbankedness” for longer and makes it harder for them to get out.
-						</p>
-					</div>
-					<div class="content placeholder">
-						<ImageLoader alt="placeholder" src="https://picsum.photos/1950/350" />
-					</div>
-				</div>
-			</div>
+			<MoreResearch />
 		{/if}
 
 		<!-- SUMMARY OF RESEARCH -->
