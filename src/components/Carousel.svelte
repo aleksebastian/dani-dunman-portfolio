03faff9c1465aab93b041784c074175b6f0a15f0
Alek <aleksebastian@gmail.com>
2022-02-28 22:59:24 -0600

    Added carousel first image preload

--- src/components/Carousel.svelte
+++ src/components/Carousel.svelte
@@ -1,4 +1,7 @@
 <script>
+	import { onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+
 	import { Navigation, Pagination, Autoplay, Keyboard } from 'swiper';
 
 	import { Swiper, SwiperSlide } from 'swiper/svelte';
@@ -12,27 +15,39 @@
 	import 'swiper/css/scrollbar';
 
 	export let carouselImgSrcs;
+	let firstImgLoaded = false;
+	onMount(() => {
+		const firstImg = new Image();
+		firstImg.src = carouselImgSrcs[0];
+		firstImg.onload = () => {
+			firstImgLoaded = true;
+		};
+	});
 </script>
 
-<Swiper
-	modules={[EffectFade, Autoplay, Keyboard, Navigation, Pagination]}
-	effect="fade"
-	slidesPerView={1}
-	pagination={{ clickable: true }}
-	navigation={true}
-	loop={true}
-	autoplay={{
-		delay: 2500,
-		disableOnInteraction: true
-	}}
-	keyboard={{
-		enabled: true
-	}}
->
-	{#each carouselImgSrcs as carouselImgSrc}
-		<SwiperSlide><img src={carouselImgSrc} alt="dani" /></SwiperSlide>
-	{/each}
-</Swiper>
+{#if firstImgLoaded}
+	<div in:fade={{ duration: 1200 }}>
+		<Swiper
+			modules={[EffectFade, Autoplay, Keyboard, Navigation, Pagination]}
+			effect="fade"
+			slidesPerView={1}
+			pagination={{ clickable: true }}
+			navigation={true}
+			loop={true}
+			autoplay={{
+				delay: 2500,
+				disableOnInteraction: true
+			}}
+			keyboard={{
+				enabled: true
+			}}
+		>
+			{#each carouselImgSrcs as carouselImgSrc}
+				<SwiperSlide><img src={carouselImgSrc} alt="dani" /></SwiperSlide>
+			{/each}
+		</Swiper>
+	</div>
+{/if}
 
 <style>
 	:root {
