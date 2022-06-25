83766ba38510a69b7260a9d69b3a25b4a615d218
Alek Ortiz <aleksebastian@gmail.com>
2022-06-24 23:59:19 -0500

    Reverted adapter and set pre-render option to true

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
+		prerender: {
+			enabled: true
+		},
 		vite: {
 			ssr: {
 				noExternal: ['chart.js']
