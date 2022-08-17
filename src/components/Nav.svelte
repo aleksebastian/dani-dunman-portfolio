a3a6e8113dbd6c45b787811a90222d9dceaabed3
Alek Ortiz <aleksebastian@gmail.com>
2022-08-17 12:05:25 -0500

    Work link scroll to project fix

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -21,7 +21,9 @@
 	});
 
 	const handleClick = () => {
-		window.scrollTo({ top: $yOffSet, behavior: 'smooth' });
+		setTimeout(() => {
+			window.scrollTo({ top: $yOffSet, behavior: 'smooth' });
+		}, 300);
 	};
 
 	let boxShadow = 'none';
